8a068c5fbd716e1919422eefff417fc8b66ad455
xiongyouqiang <657617673@qq.com>
2018-03-01 10:22:58 +0800

    1. jvm异常测试类添加一些注释

--- src/test/java/com/xyq/maventest/visualvm/StackErrorMock.java
+++ src/test/java/com/xyq/maventest/visualvm/StackErrorMock.java
@@ -1,5 +1,14 @@
 package com.xyq.maventest.visualvm;
 
+/*****
+ *模拟jvm虚拟机栈内存溢出
+ * 无限循环递归调用call方法，导致虚拟机栈的长度, 报StackOverflowError
+* @ClassName: StackErrorMock 
+* @Description: TODO(这里用一句话描述这个类的作用) 
+* @author youqiang.xiong
+* @date 2018年2月28日 上午11:40:44 
+*
+ */
 public class StackErrorMock {
     private static int index = 1;
  
